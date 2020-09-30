b971758cbfcc4524a6afcd8999b9424ee53d9e70
ultra-vi0let <threehundredthirtythird@gmail.com>
2020-09-30 01:00:00 -0400

    cosmetic changes

---
+++ layout/scripts/modalscript.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+// Gets the video src from the data-src on each button
+var $imageSrc;
+$('.gallery img').click(function() {
+    $imageSrc = $(this).data('bigimage');
+});
+console.log($imageSrc);
+
+
+
+// when the modal is opened autoplay it
+$('#myModal').on('shown.bs.modal', function (e) {
+
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+
+$("#image").attr('src', $imageSrc  );
+})
+
+
+// reset the modal image
+$('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#image").attr('src','');
+})
+
+
+
+
+
+
+// document ready
+});
